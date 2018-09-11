49483c55f67b440a08ea271c9e15aa49ed81d0ec
Tom Hill <thill@cpmagic.com>
2018-09-11 14:40:44 -0400

    Change True False Background to Yellow

--- Projects/Controllers/TrueFalseViewController.swift
+++ Projects/Controllers/TrueFalseViewController.swift
@@ -12,7 +12,7 @@ class TrueFalseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.black
+        view.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view.
     }
 
